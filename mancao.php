7400f9fc50ad1a0e9861b8a4706f8b4c04ddc76d
hubby20 <36042302+hubby20@users.noreply.github.com>
2018-02-06 04:30:38 +0800

    Add files via upload

---
+++ mancao.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<style>
+h1{
+ color:pink;
+ font-family:verdana;
+ font-size:200%;
+ text-align:center;
+}
+p1{
+ color:blue;
+ font-family:courier;
+ font-size:120%;
+}
+h2{
+ color:blue;
+ text:align:center;
+}
+p2{
+ color:green;
+
+}
+</style>
+</head>
+<body>
+
+<h1>BASIC INFORMATION</h1>
+
+<p1>Name:Mancao,Geneva S.<br>
+Address:Pitalo,San Fernando,Cebu<br>
+Email:geneva.mancao@gmail.com<br>
+Age:24 year's old<br>
+Date of Birth:May 6,1993<br>
+Gender:Female<br>
+Hobbieswatching cartoons<br>
+Favorite Color:blue and red<br>
+</p1><h2><center>EDUCATIONAL BACKGROUND</h2><p2><center>
+<table style="width:100%";color:blue;>
+<tr>
+<th>School</th>
+<th>School Address</th>
+<th>Year Graduated</th>
+</tr>
+<tr>
+<td><center>Pitalo Elementary School</center></td>
+<td><center>Pitalo,San Fernando,Cebu</center></td>
+<td><center>2005-2006</center></td>
+<tr>
+<td><center>Pitalo National High School</center></td>
+<td><center>Pitalo,San Fernando,Cebu</center></td>
+<td><center>2010-2011</center></td>
+</tr>
+</table>
+
+ <h2>Biography</h2>
+<p2><center>Every success is built on to do ability to do better then good enough. </center>
+Im Geneva S.Mancao.24 years old I live in Pitalo San Fernando,Cebu.I have two brothers and two sisters, Im the fourth among them.
+I study at Cebu Technological University.My fathers name is Felix O. Mancao he is a foreman carpenter.
+My mothers name is Elvira S.Mancao she is a plain housewife.
+My greatest fear is losing my love ones specially my family.
+My goal is to finish my study and to have a stable job in abroad.</p2>
+
+
